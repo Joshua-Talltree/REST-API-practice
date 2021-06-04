ab615ea161fd3611d108d3454e8f36a750d3208d
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-06-04 12:09:55 -0500

    feat: started events management api

---
+++ eventmanagement-api/src/main/java/com/jtalltree/eventmanagement/EventmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jtalltree.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanagementApiApplication.class, args);
+	}
+
+}
